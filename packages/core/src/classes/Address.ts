acf92f77bc09aa7d507c602f23e0f06e259b4367
ivan_pogybko <pogybkoivan@gmail.com>
2022-12-05 14:57:51 +0200

    Add Address class

---
+++ packages/core/src/classes/Address.ts
@@ -0,0 +1,53 @@
+import { fromHex, toHex } from 'tron-format-address'
+import { EMPTY_ADDRESS } from '../constants/common'
+import { IAddress } from '@jjjj_test/types/src/interfaces/Address'
+import { toChecksumAddress } from 'ethereum-checksum-address'
+
+const ERROR_ADDRESS_NOT_DEFINED = 'Tried to get not defined address'
+
+export class Address implements IAddress {
+  protected _hexAddress: string | undefined
+  protected _base58Address: string | undefined
+
+  constructor(address: string = EMPTY_ADDRESS) {
+    this.set(address)
+  }
+
+  public set(address: string = EMPTY_ADDRESS): void {
+    if (address.startsWith('0x')) {
+      this._hexAddress = toChecksumAddress(address)
+      this._base58Address = undefined
+    } else {
+      this._base58Address = address
+      this._hexAddress = undefined
+    }
+  }
+
+  public toString(): string {
+    return this.toHex()
+  }
+
+  // if needed hex format, get stored or format it from base58 format and store for future
+  public toHex(): string {
+    if (this._hexAddress) {
+      return this._hexAddress
+    }
+    if (!this._base58Address) {
+      throw ERROR_ADDRESS_NOT_DEFINED
+    }
+    const hexAddress = toChecksumAddress(toHex(this._base58Address))
+    return (this._hexAddress = hexAddress)
+  }
+
+  // if needed base58 format, get stored or format it from hex format and store for future
+  public toBase58(): string {
+    if (this._base58Address) {
+      return this._base58Address
+    }
+    if (!this._hexAddress) {
+      throw ERROR_ADDRESS_NOT_DEFINED
+    }
+    const base58Address = fromHex(this._hexAddress)
+    return (this._base58Address = base58Address)
+  }
+}
---
+++ packages/types/src/interfaces/Address.ts
@@ -0,0 +1,7 @@
+export interface IAddress {
+  toString(): string
+
+  toHex(): string
+
+  toBase58(): string
+}
