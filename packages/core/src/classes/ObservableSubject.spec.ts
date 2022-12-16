5772c8245a27e4455f123d059670b0f143949240
ivan_pogybko <pogybkoivan@gmail.com>
2022-12-16 12:53:23 +0200

    write tests

--- packages/core/src/classes/ObservableSubject.spec.ts
+++ packages/core/src/classes/ObservableSubject.spec.ts
@@ -1,9 +1,15 @@
 import { ObservableSubject } from './ObservableSubject'
 
 describe('ObservableSubject class', () => {
+  let valueListener: jest.Mock
+  let errorListener: jest.Mock
+
+  beforeEach(() => {
+    valueListener = jest.fn()
+    errorListener = jest.fn()
+  })
+
   it('check subscription', () => {
-    const valueListener = jest.fn()
-    const errorListener = jest.fn()
     const subject = new ObservableSubject()
 
     const key = 'testKey'
@@ -39,8 +45,6 @@ describe('ObservableSubject class', () => {
   })
 
   it('check error', () => {
-    const valueListener = jest.fn()
-    const errorListener = jest.fn()
     const subject = new ObservableSubject()
 
     const key = 'testKey'
@@ -59,5 +63,83 @@ describe('ObservableSubject class', () => {
 
     expect(valueListener).toBeCalledTimes(0)
     expect(errorListener).toBeCalledTimes(1)
+  })
+
+  it('check resubscribe', () => {
+    const subject = new ObservableSubject()
+
+    const key = 'testKey'
+    const listener = subject.listen(key, { next: valueListener, error: errorListener })
+
+    const testValue1 = 'testValue1'
+    subject.sendValue('testKey', testValue1)
+
+    expect(valueListener).toBeCalledTimes(1)
+    expect(valueListener).toBeCalledWith(testValue1)
+    expect(errorListener).toBeCalledTimes(0)
+
+    listener.unsubscribe()
+
+    const testValue2 = 'testValue2'
+    subject.sendValue(key, testValue2)
+
+    expect(valueListener).toBeCalledTimes(1)
+    expect(errorListener).toBeCalledTimes(0)
+
+    subject.listen(key, { next: valueListener, error: errorListener })
+
+    const testValue3 = 'testValue3'
+    subject.sendValue(key, testValue3)
+
+    expect(valueListener).toBeCalledTimes(2)
+    expect(valueListener).toBeCalledWith(testValue3)
+    expect(errorListener).toBeCalledTimes(0)
+  })
+
+  it('check multiple subscribe', () => {
+    const valueListener2 = jest.fn()
+    const errorListener2 = jest.fn()
+    const subject = new ObservableSubject()
+
+    const key = 'testKey'
+    subject.listen(key, { next: valueListener, error: errorListener })
+
+    const testValue1 = 'testValue1'
+    subject.sendValue(key, testValue1)
+
+    expect(valueListener).toBeCalledTimes(1)
+    expect(errorListener).toBeCalledTimes(0)
+    expect(valueListener).toBeCalledWith(testValue1)
+    expect(valueListener2).toBeCalledTimes(0)
+    expect(errorListener2).toBeCalledTimes(0)
+
+    subject.listen(key, { next: valueListener2, error: errorListener2 })
+
+    const testValue2 = 'testValue2'
+    subject.sendValue(key, testValue2)
+
+    expect(valueListener).toBeCalledTimes(2)
+    expect(errorListener).toBeCalledTimes(0)
+    expect(valueListener).toBeCalledWith(testValue2)
+    expect(valueListener2).toBeCalledTimes(1)
+    expect(errorListener2).toBeCalledTimes(0)
+    expect(valueListener).toBeCalledWith(testValue2)
+
+    // unsubscribes on error, and doesn't receive messages
+    const errValue1 = 'errValue1'
+    subject.sendError(key, errValue1)
+
+    expect(valueListener).toBeCalledTimes(2)
+    expect(errorListener).toBeCalledTimes(1)
+    expect(valueListener2).toBeCalledTimes(1)
+    expect(errorListener2).toBeCalledTimes(1)
+
+    const testValue3 = 'testValue3'
+    subject.sendValue(key, testValue3)
+
+    expect(valueListener).toBeCalledTimes(2)
+    expect(errorListener).toBeCalledTimes(1)
+    expect(valueListener2).toBeCalledTimes(1)
+    expect(errorListener2).toBeCalledTimes(1)
   })
 })
