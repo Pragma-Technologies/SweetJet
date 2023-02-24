c053fab277a1ccad560e2a8319133f706008ccc2
Марина Іллєнок <marinaillenok@MarinasMBP95376.netis.cc>
2023-02-24 12:33:50 +0200

    pr fix

--- packages/theme-service/src/hooks/useThemeLayoutEffect.spec.ts
+++ packages/theme-service/src/hooks/useThemeLayoutEffect.spec.ts
@@ -1,6 +1,6 @@
 import { renderHook } from '@testing-library/react-hooks'
-import { useThemeLayoutEffect } from './index'
 import { ColorConstant, ThemeName } from '../types'
+import { useThemeLayoutEffect } from './index'
 
 describe('useThemeLayoutEffect', () => {
   const lightColors: ColorConstant<string> = { primary: '#FFF', secondary: '#000' }
@@ -9,16 +9,41 @@ describe('useThemeLayoutEffect', () => {
   it('should add a style element to the head and add the light class to the body', () => {
     const themeName = 'light' as ThemeName
 
-    renderHook(() => useThemeLayoutEffect(themeName, lightColors, darkColors))
+    const { rerender } = renderHook(() => useThemeLayoutEffect(themeName, lightColors, darkColors))
+
     expect(document.head.innerHTML).toContain('body.light')
     expect(document.body.classList).toContain('light')
+
+    rerender({ themeName, lightColors, darkColors })
+
+    expect(document.head.innerHTML).toContain('body.light')
+    expect(document.body.classList).toContain('light')
+
+    rerender({ themeName: 'dark', lightColors, darkColors })
+    expect(document.head.innerHTML).toContain('body.dark')
   })
 
   it('should add a style element to the head and add the dark class to the body', () => {
     const themeName = 'dark' as ThemeName
 
-    renderHook(() => useThemeLayoutEffect(themeName, lightColors, darkColors))
+    const { rerender } = renderHook(() => useThemeLayoutEffect(themeName, lightColors, darkColors))
+
     expect(document.head.innerHTML).toContain('body.dark')
     expect(document.body.classList).toContain('dark')
+
+    rerender({ themeName, lightColors, darkColors })
+
+    expect(document.head.innerHTML).toContain('body.dark')
+    expect(document.body.classList).toContain('dark')
+
+    rerender({ themeName: 'light', lightColors, darkColors })
+    expect(document.head.innerHTML).toContain('body.light')
+  })
+
+  it('should remove all themes and add the new theme', () => {
+    renderHook(() => useThemeLayoutEffect('light', lightColors, darkColors))
+
+    expect(document.body.classList).not.toContain('dark')
+    expect(document.body.classList).toContain('light')
   })
 })
