cec9db66d8d146c6b8cb6730ad4b14969261bd67
JumboJet5 <33724740+JumboJet5@users.noreply.github.com>
2023-04-21 10:15:49 +0300

    tx list changes util hooks (#63)
    * tx list changes hooks
    
    * mark api package as private

---
+++ packages/tx-service/src/hooks/useFinishPendingTransactionEffect.ts
@@ -0,0 +1,54 @@
+import { Address, IStorable, StorageListenerTypeEnum } from '@pragma-web-utils/core'
+import { Deps, Destructor } from '@pragma-web-utils/hooks'
+import { useEffect, useRef } from 'react'
+import { pendingStatuses, useTxService } from '../core'
+import { Chain, Payload, TransactionLike } from '../types'
+import { isMultichainTx, isTransaction } from '../utils'
+
+export const useFinishPendingTransactionEffect = <P extends Payload>(
+  relationInfo: Pick<TransactionLike, 'account' | 'chainId' | 'base'>,
+  callback: () => Destructor | void,
+  filter?: (payload: P) => boolean,
+  deps: Deps = [],
+): void => {
+  const txService = useTxService<IStorable<TransactionLike<Chain, P>>>()
+  const pendingCountRef = useRef(0)
+
+  const callbackRef = useRef(callback)
+  callbackRef.current = callback
+
+  const relatedRef = useRef(relationInfo)
+  relatedRef.current = relationInfo
+
+  useEffect(() => {
+    let destructor: Destructor | void
+
+    const listener = (list: TransactionLike<Chain, P>[]): void => {
+      if (pendingCountRef.current !== list.length) {
+        destructor = !list.length ? callbackRef.current() : undefined
+      }
+      pendingCountRef.current = list.length
+    }
+
+    const _filter = (tx: TransactionLike<Chain, P>) => {
+      const isRelated = () => {
+        const { base, chainId, account } = relatedRef.current
+        return (
+          tx.base === base && tx.chainId === chainId && new Address(tx.account).toHex() === new Address(account).toHex()
+        )
+      }
+      const isFiltered = () => !filter || filter(tx.payload)
+      const isPending = isMultichainTx(tx)
+        ? pendingStatuses.has(tx.status) || pendingStatuses.has(tx.destination.status)
+        : isTransaction(tx) && pendingStatuses.has(tx.status)
+      return isPending && isFiltered() && isRelated()
+    }
+
+    const subscription = txService.addListener(StorageListenerTypeEnum.ON_LIST_CHANGES, listener, _filter)
+
+    return () => {
+      subscription.unsubscribe()
+      destructor?.()
+    }
+  }, [txService, ...deps])
+}
